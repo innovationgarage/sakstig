81871b00576b063dc23facc577e8d74a1d88a575
Egil <egil.moller@freecode.no>
2018-03-27 16:30:06 +0200

    @template() now refers to the innermost object with a '$'-expression

--- sakform/__init__.py
+++ sakform/__init__.py
@@ -31,17 +31,28 @@ def transform(data, template, debug=False, debug_path=False):
         if sakstig.is_dict(template):
             if '$' in template:
                 result = path(global_qs, local_qs, template["$"])
+                if set(template.keys()) - set(('$', "_")):
+                    result = sakstig.QuerySet(
+                        {key: value[0]
+                         for key, value in
+                         ((key, transform(global_qs, sakstig.QuerySet([item]), value))
+                          for key, value in template.items()
+                          if key not in ("$", "_"))
+                         if value}
+                        for item in result)
             else:
-                result = sakstig.QuerySet([{}])
-            if set(template.keys()) - set(('$',)):
-                result = sakstig.QuerySet(
-                    {key: value[0]
-                     for key, value in
-                     ((key, transform(global_qs, sakstig.QuerySet([item]), value))
-                      for key, value in template.items()
-                      if key != "$")
-                     if value}
-                    for item in result)
+                if set(template.keys()) - set(('$', "_")):
+                    result = sakstig.QuerySet([
+                        {key: value[0]
+                         for key, value in
+                         ((key, transform(global_qs, local_qs, value))
+                          for key, value in template.items()
+                          if key not in ("$", "_"))
+                         if value}])
+                else:
+                    result = sakstig.QuerySet([{}])
+            if '_' in template:
+                result = transform(global_qs, result, template["_"])
             return result
         elif sakstig.is_list(template):
             return sakstig.QuerySet([list(sakstig.QuerySet(
