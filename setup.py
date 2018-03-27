501d294c2ab395b1f26a71673f2620a734073513
Egil <egil.moller@freecode.no>
2018-03-27 11:30:52 +0200

    Forgot setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='sakstig',
+      version='0.1',
+      description='SakStig is an objectpath implementation that uses '
+      'proper querysets and supports querying any python object that '
+      'supports the dict or list interfaces.',
+      author='Egil Moeller',
+      author_email='egil@innovationgarage.no',
+      url='https://github.com/innovationgarage/sakstig',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          "pyleri"
+      ],
+      include_package_data=True,
+      entry_points='''
+      [console_scripts]
+      sakstig = sakstig:main
+      sakstig-grammar = sakstig.grammar:main
+      sakstig-ast = sakstig.ast:main
+      '''
+  )
