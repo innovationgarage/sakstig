d015e9fff9c99673ec898c79e9cdf873fad633b0
Egil <egil.moller@freecode.no>
2018-08-16 22:37:26 +0200

    str and array casts

--- sakstig/functions/cast.py
+++ sakstig/functions/cast.py
@@ -2,9 +2,19 @@
 
 from .. import ast_base_types
 
+def comp_str(item):
+    if item is True:
+        return "true"
+    elif item is False:
+        return "false"
+    elif item is None:
+        return "null"
+    else:
+        return str(item)
+
 class _str(ast_base_types.Function):
     def call(self, global_qs, local_qs, args):
-        return args[0].map(str)
+        return args[0].map(comp_str)
 
 class _int(ast_base_types.Function):
     def call(self, global_qs, local_qs, args):
@@ -16,4 +26,6 @@ class _float(ast_base_types.Function):
 
 class _array(ast_base_types.Function):
     def call(self, global_qs, local_qs, args):
+        if not args:
+            return ast_base_types.QuerySet([[]])
         return args[0].map(list)
