93004c79e5e403aa510b07bbbfbfcbc5f7c5f785
Egil <egil.moller@freecode.no>
2020-01-22 22:20:53 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(name='sakstig',
-      version='0.1.6',
+      version='0.1.7',
       description='SakStig is an objectpath implementation that uses '
       'proper querysets and supports querying any python object that '
       'supports the dict or list interfaces.',
